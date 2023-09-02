93d75fb294ffb886fa1ddb0ff87df5e8ec74096a
spxc <stian.instebo@gmail.com>
2023-09-02 13:28:40 +0200

    added unit tests

---
+++ __tests__/libs/download.test.js
@@ -0,0 +1,15 @@
+/**
+ * File: app.js
+ * Description: Application entry point
+ * Author: Spxc
+ * Github: https://github.com/Spxc/Stellar-blockchain-token-image-downloader
+ * Date: September 02, 2023
+ */
+
+const { fetchAsset } = require("../../src/libs/download")
+
+test('Download file', () => {
+    return fetchAsset('https://clipart-library.com/data_images/320477.png', 'GITHUB_TEST').then(data => {
+        expect(data.includes('/downloads')).toBe(true)
+    })
+})
---
+++ __tests__/libs/stellar.test.js
@@ -0,0 +1,15 @@
+/**
+ * File: app.js
+ * Description: Application entry point
+ * Author: Spxc
+ * Github: https://github.com/Spxc/Stellar-blockchain-token-image-downloader
+ * Date: September 02, 2023
+ */
+
+const { tvlTokens } = require("../../src/libs/stellar")
+
+test('Fetching token images', () => {
+    return tvlTokens(false).then(data => {
+        expect(Array.isArray(data)).toBe(true)
+    })
+})
